75325a387724fe515fa6f4cd562cb8e1ff71d681
Luc Street <6709262+0xSiO@users.noreply.github.com>
2022-01-03 16:51:25 -0800

    Set up minimal working example

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 use bolt_client::*;
-use bolt_proto::{message::*, value::*, version::*, Message, Value};
-use deadpool_bolt::Manager;
-use std::collections::HashMap;
-use deadpool::managed::Manager as DeadPoolManager;
-use tokio_util::compat::Compat;
-use tokio::io::BufStream;
-use futures::executor::block_on;
+use bolt_proto::{message::*, version::*, Value};
+use deadpool_bolt::{Manager, Pool};
 
-async fn makeClient() -> Client<Compat<BufStream<bolt_client::Stream>>> {
-  let manager = deadpool_bolt::Manager::new("localhost:7687", None, [V4_3, V4_2, 0, 0], HashMap::new().into());
-  let client: Client<Compat<BufStream<bolt_client::Stream>>> = deadpool_bolt::Manager::create(manager);
-  return client;
+async fn create_pool() -> Pool {
+    let manager = Manager::new(
+        "localhost:7687",
+        None,
+        [V4_3, V4_2, 0, 0],
+        Metadata::from_iter(vec![
+            ("user_agent", "example-client/X.Y.Z"),
+            ("scheme", "basic"),
+            // Replace these with credentials for your DB
+            ("principal", "neo4j"),
+            ("credentials", "neo4j"),
+        ]),
+    )
+    .await
+    .unwrap();
+
+    Pool::builder(manager).max_size(15).build().unwrap()
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    // Share this pool across your route handlers
+    let pool: Pool = create_pool().await;
 
-  let client = block_on(makeClient);
+    // Do this in your route handler
+    let mut client = pool.get().await.unwrap();
 
-  println!("Hello, world!");
+    // Use the connection however you wish
+    client.run("RETURN 42 AS num;", None, None).await.unwrap();
+    let (records, summary) = client
+        .pull(Some(Metadata::from_iter(vec![("n", 1)])))
+        .await
+        .unwrap();
+    println!("{:?}", summary);
+    println!("{:?}", records);
+
+    assert!(Success::try_from(summary).is_ok());
+    assert_eq!(records[0].fields(), &[Value::from(42)]);
+
+    // When the connection is dropped, it will be returned to the pool and reset
 }
